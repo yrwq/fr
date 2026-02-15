d66a0853ee6b2881db42332d4913c26d2d38d956
yrwq <yrwqid@gmail.com>
2026-02-15 06:26:04 +0100

    is dot dir

--- main.c
+++ main.c
@@ -7,7 +7,15 @@ static int is_dir(const char *path) {
     return stat(path, &st) == 0 && S_ISDIR(st.st_mode);
 }
 
+// check if dir is "." or ".."
+static int is_dot_dir(const char *path) {
+    if (!is_dir(path)) return 0;
+    return path[0] == '.' &&
+        (path[1] == '\0' || (path[1] == '.' && path[2] == '\0'));
+}
+
 int main() {
-    printf("%d\n", is_dir("."));
+    printf("%d\n", is_dot_dir("."));
+    printf("%d\n", is_dot_dir("/"));
     return 0;
 }
