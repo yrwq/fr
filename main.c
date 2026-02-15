0ff7779e884b77ab1483731dc8f10a667beb042a
yrwq <yrwqid@gmail.com>
2026-02-15 06:52:20 +0100

    find repos

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
-#include <linux/limits.h>
+#define _XOPEN_SOURCE 700
+#define _POSIX_C_SOURCE 200809L
+
+#include <limits.h>
 #include <stddef.h>
 #include <stdio.h>
-
+#include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
 static int is_dir(const char *path) {
@@ -11,11 +16,11 @@ static int is_dir(const char *path) {
 }
 
 // check if dir is "." or ".."
-// static int is_dot_dir(const char *path) {
-//     if (!is_dir(path)) return 0;
-//     return path[0] == '.' &&
-//         (path[1] == '\0' || (path[1] == '.' && path[2] == '\0'));
-// }
+static int is_dot_dir(const char *path) {
+    if (!is_dir(path)) return 0;
+    return path[0] == '.' &&
+        (path[1] == '\0' || (path[1] == '.' && path[2] == '\0'));
+}
 
 // safely built path
 static int build_path(
@@ -27,17 +32,48 @@ static int build_path(
     return 1;
 }
 
+// check if directory is a git repo
 static int is_repo(const char *path) {
     char buf[PATH_MAX];
     if (!build_path(buf, sizeof buf, path, ".git")) return 0;
     return is_dir(buf);
 }
 
+// recursively find repos in dir tree
+static void find_repos(const char *path, size_t len) {
+    DIR *dir = opendir(path);
+    if (!dir) return;
+
+    for (struct dirent *ent; (ent = readdir(dir)) != NULL;) {
+        if (is_dot_dir(ent->d_name)) continue;
+
+        char child[PATH_MAX];
+        if (!build_path(child, sizeof child, path, ent->d_name)) continue;
+
+        if (!is_dir(child)) continue;
+
+        if (is_repo(child)) {
+            printf("%s\n", child);
+        } else {
+            find_repos(child, len);
+        }
+    }
+    closedir(dir);
+}
+
 
 int main() {
-    printf("%d\n", is_repo("../fr"));
-    printf("%d\n", is_repo("."));
-    printf("%d\n", is_repo(getenv("HOME")));
+    char start[PATH_MAX];
 
+    const char *home = getenv("HOME");
+    snprintf(start, sizeof start, "%s/dev", home);
+
+    char can[PATH_MAX];
+
+    if (!realpath(start, can)) {
+        return 0;
+    }
+
+    find_repos(can, strlen(can));
     return 0;
 }
