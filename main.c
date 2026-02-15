1aa42aa852a1dbc5eca0c4801de2399bffc26b74
yrwq <yrwqid@gmail.com>
2026-02-15 06:22:23 +0100

    is directory

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+#include <sys/stat.h>
+
+static int is_dir(const char *path) {
+    struct stat st;
+    return stat(path, &st) == 0 && S_ISDIR(st.st_mode);
+}
+
 int main() {
-    printf("hello world\n");
+    printf("%d\n", is_dir("."));
     return 0;
 }
